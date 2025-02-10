ec3d9caf290c0dd3c84cb34d51b9741c96fb7033
Amine Azizi <hi@amineazizi.com>
2025-02-10 20:24:17 +0100

    feat: add singleton pattern

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const instance1 = Singleton.getInstance();
 const instance2 = Singleton.getInstance();
 console.log(instance1 !== instance2); // true
 
-var a = Singleton.getInstance();
+const a = Singleton.getInstance();
 const b = Singleton.getInstance();
 
 console.log(a === b);
